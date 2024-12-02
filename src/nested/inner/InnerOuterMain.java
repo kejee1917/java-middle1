120582e57e53b7eb5df555c89177edbf325a8969
kejee1917 <kejee1917@gmail.com>
2024-12-02 22:22:22 +0900

    [section7] 중첩 클래스/내부 클래스 (리팩토링)

--- src/nested/inner/InnerOuterMain.java
+++ src/nested/inner/InnerOuterMain.java
@@ -5,6 +5,7 @@ public class InnerOuterMain {
     public static void main(String[] args) {
         InnerOuter outer = new InnerOuter();
         InnerOuter.Inner inner = outer.new Inner();
+        //new InnerOuter().Inner(); 이렇게 생성 못함.
 
         System.out.println(inner.getClass());
     }
